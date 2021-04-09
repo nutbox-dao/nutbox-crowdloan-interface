8813ecedaea7dc6588970643e346537baef1cbf8
somiar <somiar2010@hotmail.com>
2021-04-09 14:24:02 +0800

    样式修改

--- src/components/Buttons/ConnectWallet.vue
+++ src/components/Buttons/ConnectWallet.vue
@@ -1,48 +1,40 @@
 <template>
-    <div>   
-        <button @click="connect" :style="style" id="connect">
-            Connect Wallet
-        </button>
-    </div>
+  <div>
+    <button @click="connect" class="primary-btn">
+      Connect Wallet
+    </button>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'ConnectWallet',
-        props: {
-            width: {
-                type: Number,
-                default: 160
-            },
-            height: {
-                type: Number,
-                default: 38
-            },
-            radius: {
-                type: Number,
-                default: 8
-            }
-        },
-        computed: {
-            style() {
-                return "width:"+this.width+'px;' + 'height:' + this.height + 'px;' + 'border-radius:'+ this.radius + 'px;'
-            }
-        },
-        methods: {
-            connect() {
-                
-            }
-        },
+export default {
+  name: 'ConnectWallet',
+  props: {
+    width: {
+      type: Number,
+      default: 160
+    },
+    height: {
+      type: Number,
+      default: 38
+    },
+    radius: {
+      type: Number,
+      default: 8
     }
+  },
+  computed: {
+    style () {
+      return 'width:' + this.width + 'px;' + 'height:' + this.height + 'px;' + 'border-radius:' + this.radius + 'px;'
+    }
+  },
+  methods: {
+    connect () {
+
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
-#connect{
-    border: none;
-    background-color: blanchedalmond;
-}
-#connect:hover{
-    cursor: pointer;
-    background-color: rgb(250, 225, 200);
-}
 </style>
